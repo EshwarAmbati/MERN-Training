0d0d2962ab0c184168ae5bca1188d210d633283e
EshwarAmbati <postbox.eshwar@gmail.com>
2024-07-28 11:29:59 +0530

    c

--- CLOUDHOME/frontend/appRouter.js
+++ CLOUDHOME/frontend/appRouter.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
 import LoginPage from "./src/pages/loginPage.js";
-import SignupPage from "./src/pages/signupPage.js";
+import SignupPage from "./src/pages/SignupPage.js";
 import { useSelector } from "react-redux";
 import HomePage from "./src/pages/homePage.js";
 import OtpPage from "./src/pages/otpPage.js";
