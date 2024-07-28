ac82f825bf94cbfdb8ac7284cb2cb6891538381c
EshwarAmbati <postbox.eshwar@gmail.com>
2024-07-28 11:27:20 +0530

    last commit

--- CLOUDHOME/frontend/appRouter.js
+++ CLOUDHOME/frontend/appRouter.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { createBrowserRouter, Navigate, RouterProvider } from "react-router-dom";
-import LoginPage from "./src/pages/loginPage";
-import SignupPage from "./src/pages/signupPage";
+import LoginPage from "./src/pages/loginPage.js";
+import SignupPage from "./src/pages/signupPage.js";
 import { useSelector } from "react-redux";
-import HomePage from "./src/pages/homePage";
-import OtpPage from "./src/pages/otpPage";
+import HomePage from "./src/pages/homePage.js";
+import OtpPage from "./src/pages/otpPage.js";
 
 const AppRouter = () => {
     const { isAuthorized, isEmailVerified } = useSelector((e) => e.auth);
